537790f5d4cb18b0b714825ccba2607f2bcf5e90
L1m1tz <andreew.mk@gmail.com>
2022-05-10 09:11:03 +0200

    characters

---
+++ src/components/Characters.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<script>
+//import axios from "axios";
+import { ref } from "vue";
+
+//import StarWarsAPI from "./services/StarWarsAPI";
+
+export default {
+  setup() {
+    const quote = ref("");
+
+    const loadQuote = async () => {
+      const response = await StarWarsAPI.getData();
+      quote.value = response;
+    };
+    loadQuote();
+    return {
+      quote
+    };
+  },
+ 
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button @click="onClick" text="Add Task" color="red" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+  components: {},
+
+  methods: {
+    onClick() {},
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
